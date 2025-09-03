bb39ab5f2dc841518281dbd9b372eb2cdf7a5415
S. M. Tahmeed Reza <tahmeed.reza@dsinnovators.com>
2025-09-03 15:50:51 +0600

    Added New Game Menu

--- game_1.cpp
+++ game_1.cpp
@@ -2,16 +2,46 @@
 #include"dependencies.h"
 
 int main() {
+	const char* newMenuItems[] = {
+		"Reboot game -> press x",
+		"Start game -> press 1",
+		"Credits -> press 2",
+		"Quit game -> press Esc",
+	}; 
+
 start_game:
 	printWelcomeMessage();
-	goto game_loop;
 
-game_loop:
+new_game_menu:
+	printMenuMessage(newMenuItems, 3);
+	goto n_menu_update;
+
+game_menu:
+	printMenuMessage(newMenuItems, 3);
+	goto menu_update
+
+n_menu_update:
 	char ch = getch();
-	if (ch == 27) {
-		goto end_game;	
+	switch(ch){
+		case 27:
+			goto end_game;
+			break;
+		case '1':
+			goto game_loop;
+			break;
+		case 'x':
+			goto start_game;
+			break;
+		case '2':
+			printCredits();
+			goto new_game_menu;
+			break;
+		default:
 	}
-	goto game_loop;
+	goto n_menu_update;
+	
+game_loop:
+	std::cout << "Game Loop Encountered" << std::endl;
 	
 end_game:
 	printClosingMessage();
